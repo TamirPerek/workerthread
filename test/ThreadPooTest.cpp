6f3d70d340e0a5d6b4fdfbd074fd83470d4d79da
Nils Brödner <nilsbroedner@me.com>
2021-11-08 00:06:27 +0100

    add some useless tests

--- test/ThreadPooTest.cpp
+++ test/ThreadPooTest.cpp
@@ -11,19 +11,32 @@ vdk::signal<void(int)> gResult;
 std::atomic<int> tResult{0};
 
 void resultFunction(int xResult);
+TEST(ThreadPoolTest, AddOneFunctionToPool)
+{
+    WorkerThread::ThreadPool::getInstance().AddToPool([]()
+                                                      { std::cout << "I'm a thread\n"; });
+}
+TEST(ThreadPoolTest, AddMultipleFunctionsToPool)
+{
+    for (std::size_t i = 0; i < 100; i++)
+    {
+        WorkerThread::ThreadPool::getInstance().AddToPool([i]()
+                                                          { std::cout << "I'm a thread number: " << i << '\n'; });
+    }
+}
 
 TEST(ThreadPoolTest, Test)
 {
-    
+
     gResult.connect(&resultFunction);
-    
-    for(std::size_t i = 0; i < 10; i++)
+
+    for (std::size_t i = 0; i < 10; i++)
     {
-        WorkerThread::ThreadPool::getInstance().AddToPool([i]() {
+        WorkerThread::ThreadPool::getInstance().AddToPool([i]()
+                                                          {
             std::this_thread::sleep_for(std::chrono::seconds(10 + i));
             std::cout << "finsihed: " << i << " after: " << 10 + i << "s \n";
-            gResult.emit(i);
-        });
+            gResult.emit(i); });
     }
     std::this_thread::sleep_for(std::chrono::seconds(60));
     EXPECT_EQ(tResult, 45);
